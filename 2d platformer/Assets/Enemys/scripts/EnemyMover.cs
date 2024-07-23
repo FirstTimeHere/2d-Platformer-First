d32a6e428ef7ddc9697d47fb3fa782dcfbc8b343
FirstTimeHere <97403208+FirstTimeHere@users.noreply.github.com>
2024-07-23 15:36:07 +0500

    Add files via upload

--- 2d platformer/Assets/Enemys/scripts/EnemyMover.cs
+++ 2d platformer/Assets/Enemys/scripts/EnemyMover.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 [RequireComponent(typeof(EnemyAnimator))]
 public class EnemyMover : MonoBehaviour
 {
-    private float _speed;
-
     private Border _borderBegin, _borderEnd;
 
     private bool _isItBorderBegin;
+
+    private float _speed;
 
     private Vector2 _negativeScale;
     private Vector2 _defaultScale;
@@ -30,24 +30,45 @@ public class EnemyMover : MonoBehaviour
 
     private void Move()
     {
-        if (_isItBorderBegin)
+        if (SeePlayer(out Transform target))
         {
-            transform.position = Vector2.MoveTowards(transform.position, _borderEnd.transform.position, _speed * Time.deltaTime);
-            transform.localScale = _negativeScale;
+            transform.position = Vector2.MoveTowards(transform.position, target.position, _speed * Time.deltaTime);
         }
         else
         {
-            transform.position = Vector2.MoveTowards(transform.position, _borderBegin.transform.position, _speed * Time.deltaTime);
-            transform.localScale = _defaultScale;
+            if (_isItBorderBegin)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, _borderEnd.transform.position, _speed * Time.deltaTime);
+                transform.localScale = _negativeScale;
+            }
+            else
+            {
+                transform.position = Vector2.MoveTowards(transform.position, _borderBegin.transform.position, _speed * Time.deltaTime);
+                transform.localScale = _defaultScale;
+            }
         }
 
         Ran?.Invoke(true);
     }
 
-    public void GetBorders(Border border, Border secondBorder)
+    private bool SeePlayer(out Transform target)
     {
-        _borderBegin = border;
-        _borderEnd = secondBorder;
+        Vector3 direction = _borderBegin.transform.position - _borderEnd.transform.position;
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, direction);
+
+        foreach (RaycastHit2D hit2D in hits)
+        {
+            if (hit2D.rigidbody.gameObject.GetComponent<Player>())
+            {
+                target = hit2D.collider.gameObject.transform;
+                Debug.Log(target);
+                return true;
+            }
+        }
+
+        target = null;
+
+        return false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -64,6 +85,12 @@ public class EnemyMover : MonoBehaviour
 
     public void GetSpeed(float speed)
     {
-       _speed = speed;
+        _speed = speed;
+    }
+
+    public void GetBorders(Border border, Border secondBorder)
+    {
+        _borderBegin = border;
+        _borderEnd = secondBorder;
     }
 }
